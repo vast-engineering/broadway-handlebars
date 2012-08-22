563fe34cca37245da7729db1c5b0f9971549ff81
Tommy Messbauer <tommy@vast.com>
2012-08-22 16:26:03 -0500

    Update for partial walking

--- viewresolver.js
+++ viewresolver.js
@@ -4,7 +4,8 @@ var _ = require('lodash'),
 
 var ViewResolver = function(options) {
 	var defaults = {
-		base: process.cwd() + '/'
+		base: process.cwd() + '/',
+		ext: "html"
 	};
 	this.options = _.extend(defaults, options);
 };
@@ -15,6 +16,19 @@ var ViewResolver = function(options) {
 **/
 ViewResolver.prototype.get = function(name, callback) {
 	fs.readFile(this.options.base + name, 'utf-8', callback);
+};
+
+/**
+* Retrieves the markup for all views in a given folder.  
+* Return will be in format of 
+*    key: path relative to the base
+*    val: the contents of the file
+**/
+ViewResolver.prototype.all = function(callback) {
+	var dict = {};
+
+	// walk the folder and return an object with all
+	return dict;
 };
 
 module.exports = ViewResolver;
